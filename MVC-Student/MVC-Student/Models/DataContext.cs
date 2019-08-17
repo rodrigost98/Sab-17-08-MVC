8ef4af92f9090852ad1660601e873f7395ce510e
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 08:49:35 -0400

    Con MVC comlpeto

---
+++ MVC-Student/MVC-Student/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVC_Student.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVC_Student.Models.Student> Students { get; set; }
+    }
+}
---
+++ MVC-Student/MVC-Student/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Student.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+        [Required]
+        [Display(Description ="Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Description ="First Name")]
+        public string FirstName { get; set; }
+      
+    }
+}
